96ff9ec642ceac2c89ee2d2f837438fe4f656fbe
Andrey <aa7155443@gmail.com>
2022-05-30 16:29:26 +0300

    add new file

--- индивидуальные/2i.py
+++ индивидуальные/2i.py
@@ -4,15 +4,24 @@
 if __name__ == '__main__':
 
     a = list(map(int, input().split()))
+
     C = int(input("Введите число"))
     t = 0
     for i in a:
         if i > C:
             t += 1
-    answ = 1
-    for i in a[a.index(max(a, key=abs)) + 1:]:
-        answ *= i
     print(t)
+
+    answ = 1
+    i_max = 0
+    a_max = a[0]
+    for i, item in enumerate(a):
+        if abs(item) > abs(a_max):
+            i_max, a_max = i, item
+
+    for item in a[i_max+1:]:
+        answ *= item
     print(answ)
+
     a.sort(key=lambda x: x >= 0)
     print(a)
